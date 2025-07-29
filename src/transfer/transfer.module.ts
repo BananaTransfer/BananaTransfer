b285b6831b4e29fe084954faa2d1e7f43debf2c4
Dominik Saul <dominik@saul.ch>
2025-07-29 18:41:20 +0200

    fix imports in remote

--- src/transfer/transfer.module.ts
+++ src/transfer/transfer.module.ts
@@ -1,32 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '@user/user.module';
 
 import { TransferController } from '@transfer/controllers/transfer.controller';
 import { TransferService } from '@transfer/services/transfer.service';
-import { UserService } from '@user/services/user.service';
 
-import { User } from '@database/entities/user.entity';
-import { LocalUser } from '@database/entities/local-user.entity';
-import { RemoteUser } from '@database/entities/remote-user.entity';
-import { TrustedRecipient } from '@database/entities/trusted-recipient.entity';
 import { FileTransfer } from '@database/entities/file-transfer.entity';
 import { TransferLog } from '@database/entities/transfer-log.entity';
 
 // This module handles all file sharing related operations that are done by the authenticated users
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      User,
-      LocalUser,
-      RemoteUser,
-      TrustedRecipient,
-      FileTransfer,
-      TransferLog,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([FileTransfer, TransferLog]), UserModule],
   controllers: [TransferController],
-  providers: [TransferService, UserService],
+  providers: [TransferService],
   exports: [TransferService],
 })
 export class TransferModule {}
