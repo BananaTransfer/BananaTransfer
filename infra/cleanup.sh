f057c805eb05e75a0c37dc26c2482fa4c47460b0
ansermgw <25158748+ansermgw@users.noreply.github.com>
2025-08-10 11:03:36 +0200

    fix: remove shutdown since in make terraform destroy the instance during the next execution due to state drift

--- infra/cleanup.sh
+++ infra/cleanup.sh
@@ -8,8 +8,7 @@ sudo bash -c "
 cd /opt/app && \
 aws s3 rm \"s3://$BUCKET/\" --recursive && \
 docker compose down && \
-docker volume rm app_pg_data && \
-(sleep 10 && shutdown -h now) & # hack to not get an error code as a return of this script when connection is killed by server
+docker volume rm app_pg_data
 "
 EOF
 
