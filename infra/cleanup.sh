ad721bdc36a825f36a66eb1de2b32a4e97f2d91c
ansermgw <25158748+ansermgw@users.noreply.github.com>
2025-08-10 10:31:30 +0200

    chore: shutdown e2e test instance after pipeline to save cost

--- infra/cleanup.sh
+++ infra/cleanup.sh
@@ -8,7 +8,8 @@ sudo bash -c "
 cd /opt/app && \
 aws s3 rm \"s3://$BUCKET/\" --recursive && \
 docker compose down && \
-docker volume rm app_pg_data
+docker volume rm app_pg_data && \
+(sleep 10 && shutdown -h now) & # hack to not get an error code as a return of this script when connection is killed by server
 "
 EOF
 
