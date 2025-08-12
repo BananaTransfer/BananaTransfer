3b42a16e4510842449d36f8f344f7f8d22f0f6dd
Dominik Saul <dominik@saul.ch>
2025-08-12 14:52:40 +0200

    implement password change date at

---
+++ src/database/migrations/1755002812424-passwordcreatedat.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Passwordcreatedat1755002812424 implements MigrationInterface {
+    name = 'Passwordcreatedat1755002812424'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "password_created_at" TIMESTAMP DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "last_login" SET DEFAULT now()`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "last_login" DROP DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password_created_at"`);
+    }
+
+}
