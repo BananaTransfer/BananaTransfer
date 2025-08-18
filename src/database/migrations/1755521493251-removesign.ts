4fe2c6cc3137e60bd56783dca66d663799f7a18a
Dominik Saul <dominik@saul.ch>
2025-08-18 15:57:52 +0200

    fix e2e tests after redirection to user set key page if no keys configured

--- src/database/migrations/1755521493251-removesign.ts
+++ src/database/migrations/1755521493251-removesign.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Removesign1755521493251 implements MigrationInterface {
-    name = 'Removesign1755521493251'
+  name = 'Removesign1755521493251';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "file_transfer" DROP COLUMN "signature_sender"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "file_transfer" DROP COLUMN "signature_sender"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "file_transfer" ADD "signature_sender" text NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "file_transfer" ADD "signature_sender" text NOT NULL`,
+    );
+  }
 }
